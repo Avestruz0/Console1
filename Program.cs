e221f67c557e4ac52eac0bdfd5bcb274bde921fd
Lucas Campos <lucas10989316@edu.pa.senac.br>
2024-06-13 21:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Console1
+{
+    internal class Program { 
+    
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario();
+
+            usuario.nome = "Nickolas";
+            usuario.login = "root";
+            usuario.senha = "Senac123#";
+
+            //Console.WriteLine("Hello, " + usuario.nome);
+            //Console.WriteLine(usuario.logar("root", "qwe123"));
+            //string result = Console.ReadLine();
+            //Método para digitar
+            //Console.WriteLine("O usuário digitou: " + result);
+
+            //do{
+            // Console.WriteLine("Digite seu login: ");
+            //string login = Console.ReadLine();
+
+            //Console.WriteLine("Digite sua senha: ");
+            //string senha = Console.ReadLine();
+
+            // Console.WriteLine(usuario.logar(login, senha));
+            // } while (true);
+
+
+            string login;
+            string senha;
+            do
+            {
+                Console.WriteLine("Digite seu login: ");
+                login = Console.ReadLine();
+
+                Console.WriteLine("Digite sua senha: ");
+                senha = Console.ReadLine();
+
+
+            } while (!usuario.logar(login, senha));
+
+
+
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    internal class Usuario {
+        //nome, login, senha
+        public string nome;
+        public string login;
+        public string senha;
+        public int qtdLogin;
+        public float peso;
+        public bool ehSolteiro;
+        public char abrvNomeMeio;
+
+        public bool logar(string _login, string _senha) {
+
+            if (_login == login){
+                if (_senha == senha) {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
